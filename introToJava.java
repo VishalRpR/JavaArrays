eccfc10d57360451935924e66c6da4ab6bd27dbc
vishal <vishrrajput02@gmail.com>
2023-06-23 18:33:11 +0530

    intro to array

---
+++ introToJava.java
@@ -0,0 +1,20 @@
+
+public class introToJava {
+
+	public static void main(String[] args) {
+	   int[] arr;
+	   arr=new int[5];
+	   arr[0]=54;
+	   arr[1]=77;
+	   arr[2]=87;
+	   arr[3]=65;
+	   arr[4]=43;
+	   
+//	   System.out.println(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
