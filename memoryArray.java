c2b816b0d68fe17cf186c0ab3213662b9d1e78e7
vishal <vishrrajput02@gmail.com>
2023-06-24 13:14:24 +0530

    swap with fxn

--- memoryArray.java
+++ memoryArray.java
@@ -1,6 +1,6 @@
 
 public class memoryArray {
-
+/*
 	public static void main(String[] args) {
 
 			
@@ -15,6 +15,10 @@ public class memoryArray {
 			   
 			   int [] two=arr;
 			   two[2]=870;
+			   
+			   
+			   //for arr and two same output
+			   
 		        for (int i = 0; i < arr.length; i++) {
 					System.out.println(arr[i]);
 		
@@ -30,5 +34,31 @@ public class memoryArray {
 
 
 	}
+	*/
+	public static void swap(int[] arr, int i, int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+		
+	}
+	
+	public static void main(String[] args) {
+
+		
+		   int[] arr;
+		   arr=new int[5];
+		   arr[0]=54;
+		   arr[1]=77;
+		   arr[2]=87;
+		   arr[3]=65;
+		   arr[4]=43;
+		   
+		swap(arr,0,4);
+         for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+
+
+}
 
 }
