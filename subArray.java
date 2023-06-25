c3a42e6f195ef07deffcfaf187f345ad244cbf1a
vishal <vishrrajput02@gmail.com>
2023-06-25 16:11:35 +0530

    subarray problem

---
+++ subArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class subArray {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n =scn.nextInt();
+		int[] arr=new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=scn.nextInt();
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr.length; j++) {
+				for (int j2 = i; j2 <=j; j2++) {
+					System.out.print(arr[j2]);
+				}
+				System.out.println();
+			}
+		}
+	}
+}
